4afa45d473ab37b6a524eca2c20446d7986ecd99
miguelllanosi <dellanomike@gmail.com>
2020-08-14 19:48:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -262,7 +262,13 @@
                                             <input type="hidden" name="title" value="Samsung Galaxy S9">
                                             <input type="hidden" name="price" value="15000">
                                             <input type="hidden" name="unit" value="1">
-                                            <button type="submit" class="mercadopago-button" formmethod="post">Comprar</button>
+                                            <button type="submit" class="mercadopago-button" formmethod="post"><form action="/procesar-pago" method="POST">
+  <script
+   src="https://www.mercadopago.com.mx/integrations/v1/web-payment-checkout.js"
+   data-preference-id="<?php echo $preference->id; ?>">
+  </script>
+</form>Comprar</button>
+                                            
                                         </form>
                                     </div>
                                 </div>
